063a0a10f54ed9d2e48c2fa0da2df54e2dee26aa
stanondieki <ondiekistanley21@gmail.com>
2024-11-28 12:00:40 +0300

    med_relief commit

--- med_relief-frontend/src/App.js
+++ med_relief-frontend/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './components/login';
+import './index.css';
 //import Signup from './components/Signup';
 //import Dashboard from './components/Dashboard';
 //import Payout from './components/Payout';
-
 const App = () => {
   return (
-    // <Router>
-      {/* <Routes>
+     <Router>
+       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      {/*   <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/payout" element={<Payout />} />
-      </Routes> */}
-    // </Router>
+        <Route path="/payout" element={<Payout />} />*/}
+      </Routes> 
+     </Router>
   );
 };
 
